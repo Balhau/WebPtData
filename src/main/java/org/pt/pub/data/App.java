5f6a7c29dc17e88bbe8a0861a4a3b1d543d4b7f7
Balhau <balhau@balhau.net>
2013-11-23 12:16:24 +0000

    Initial commit

---
+++ src/main/java/org/pt/pub/data/App.java
@@ -0,0 +1,13 @@
+package org.pt.pub.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
