1be8d3d8fa013018b4668fa7ce51f9b2644dec80
Vitor Fernandes <vitor.fernandes@blip.pt>
2015-06-30 17:44:43 +0100

    Added landweather domain object

---
+++ src/main/java/org/pt/pub/data/sources/ipma/domain/LandWeather.java
@@ -0,0 +1,10 @@
+package org.pt.pub.data.sources.ipma.domain;
+
+/**
+ * Domain object that represents the weather relative to a specific geographic location
+ * @author balhau
+ *
+ */
+public class LandWeather {
+
+}
