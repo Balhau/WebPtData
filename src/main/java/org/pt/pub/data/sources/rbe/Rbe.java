7e7670130ed654d7b6fc9a8621544f871e4062a4
Vitor Fernandes <vitor.fernandes@blip.pt>
2015-07-07 15:23:59 +0100

    Added rede de bibliotecas escolares service

---
+++ src/main/java/org/pt/pub/data/sources/rbe/Rbe.java
@@ -0,0 +1,12 @@
+package org.pt.pub.data.sources.rbe;
+
+
+/**
+ * Rede de Bibliotecas Escolares (RBE).
+ * This class will be used to retrieve information from http://rbe.mec.pt/
+ * @author balhau 
+ *
+ */
+public class Rbe {
+	
+}
