db9a2e44e66fe6c5db1890c49669481dfcbe0f80
balhau <balhau@balhau.net>
2015-07-17 23:43:57 +0100

    Add CP and TAP skeleton

---
+++ src/main/java/org/pt/pub/data/sources/tap/TAP.java
@@ -0,0 +1,11 @@
+package org.pt.pub.data.sources.tap;
+
+/**
+ * API for the Transportes Aéreos Portugueses or TAP until recently a public institution that operates
+ * in the flight markets. With this API you can search for flights, get the time and prices for each one
+ * @author balhau
+ *
+ */
+public class TAP {
+
+}
