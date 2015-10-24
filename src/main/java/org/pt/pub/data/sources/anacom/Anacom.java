71aab324bc0bc48977c2b8a3adfbf08be3d4ecbb
Vitor Fernandes <vitor.fernandes@blip.pt>
2015-10-24 11:19:09 +0100

    Added anacom services for mobile services and not so mobile services...

--- src/main/java/org/pt/pub/data/sources/anacom/Anacom.java
+++ src/main/java/org/pt/pub/data/sources/anacom/Anacom.java
@@ -5,7 +5,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.pt.pub.data.sources.anacom.domain.TarifaFixo;
 import org.pt.pub.data.sources.anacom.domain.TarifaInternet;
+import org.pt.pub.data.sources.anacom.domain.TarifaMovel;
 import org.pt.pub.data.sources.anacom.domain.TarifaTelevisao;
 import org.pt.pub.global.configs.GlobalConfigs;
 
@@ -26,6 +28,8 @@ public class Anacom {
     public static String ANACOM_BASE="http://www.anacom.pt";
     public static String TARIFARIOS_INTERNET=ANACOM_BASE+"/tarifarios/InternetResultadosConsultaTodos.do";
     public static String TARIFARIOS_TELEVISAO=ANACOM_BASE+"/tarifarios/TelevisaoResultadosConsultaTodos.do";
+    public static String TARIFARIOS_MOVEL=ANACOM_BASE+"/tarifarios/MovelResultadosConsultaTodos.do";
+    public static String TARIFARIOS_FIXO=ANACOM_BASE+"/tarifarios/FixoResultadosConsultaTodos.do";
 
     /**
      * Return all the internet services available from the Portuguese Market
@@ -38,6 +42,14 @@ public class Anacom {
 
     public List<TarifaTelevisao> getTarifariosTelevisao() throws Exception{
         return processTable(TARIFARIOS_TELEVISAO,(Element line)-> getTarifaTelevisao(line));
+    }
+
+    public List<TarifaMovel> getTarifariosMovel() throws Exception{
+        return processTable(TARIFARIOS_MOVEL,(Element line) -> getTarifaMovel(line));
+    }
+
+    public List<TarifaFixo> getTarifariosFixo() throws Exception{
+        return processTable(TARIFARIOS_FIXO,(Element line) -> getTarifaFixo(line));
     }
 
 
@@ -56,6 +68,33 @@ public class Anacom {
     private TarifaTelevisao getTarifaTelevisao(Element element){
         Elements cells=element.getElementsByTag("td");
         return new TarifaTelevisao(
+                cells.get(1).text(),
+                cells.get(2).text(),
+                cells.get(6).text(),
+                cells.get(3).text(),
+                cells.get(4).text(),
+                cells.get(5).text()
+        );
+    }
+
+    private TarifaMovel getTarifaMovel(Element element){
+        Elements cells=element.getElementsByTag("td");
+
+        return new TarifaMovel(
+                cells.get(1).text(),
+                cells.get(2).text(),
+                cells.get(7).text(),
+                cells.get(3).text(),
+                cells.get(4).text(),
+                cells.get(5).text(),
+                cells.get(6).text()
+        );
+    }
+
+    private TarifaFixo getTarifaFixo(Element element){
+        Elements cells=element.getElementsByTag("td");
+
+        return new TarifaFixo(
                 cells.get(1).text(),
                 cells.get(2).text(),
                 cells.get(6).text(),
